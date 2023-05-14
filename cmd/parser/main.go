7c96cb8658a5ccff8a14ea1eccef5796fea4e44f
Iliya Iliev <iliyailiev3592@gmail.com>
2023-05-14 10:24:31 +0300

    cmd: parser: Add udp filter
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"flag"
 	"os/signal"
 	"syscall"
 
@@ -12,14 +13,23 @@ import (
 )
 
 func main () {
-	batch_size := uint32(30)
+	// Define flags
+	net_iface := flag.String("net_iface", xdpiface.XDP_IFACE_DEFAULT, "First argument")
+	batch_size := flag.Int("batch_size", 30, "Batch Size")
+
+	// Parse command-line arguments
+	flag.Parse()
+
+	// Access the flag values
+	fmt.Println("Net Interface:", *net_iface)
+	fmt.Println("Batch size:", *batch_size)
+
 	var frames_rcvd uint32 = 0
 	var iBufferSize int = 0
 	iBuffer := make([]byte, 9000)
 
 	xdpiface.XdpLogLevelSet(xdpiface.XDP_LOG_INFO)
-
-	xdp_iface, err := xdpiface.XdpIfaceNew(xdpiface.XDP_IFACE_DEFAULT)
+	xdp_iface, err := xdpiface.XdpIfaceNew(*net_iface)
 	if err != nil {
 		fmt.Println("Failed to create XDP iface", err)
 	}
@@ -37,7 +47,7 @@ func main () {
 
     xdp_sock.SetSockopt(xdpiface.SO_PREFER_BUSY_POLL, 1)
     xdp_sock.SetSockopt(xdpiface.SO_BUSY_POLL, 20)
-    xdp_sock.SetSockopt(xdpiface.SO_BUSY_POLL_BUDGET, int(batch_size))
+    xdp_sock.SetSockopt(xdpiface.SO_BUSY_POLL_BUDGET, *batch_size)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
@@ -49,22 +59,32 @@ func main () {
 	    stop = true
 	}()
 
+	var udp_dgrams uint64 = 0
 	for {
-	    xdp_sock.RxBatchGetSize (&frames_rcvd, batch_size);
-	    fmt.Println("Frames received: ", frames_rcvd)
+	    xdp_sock.RxBatchGetSize (&frames_rcvd, uint32(*batch_size));
 		for i := 1; i <= int(frames_rcvd); i ++ {
 	        xdp_sock.Recv (iBuffer, &iBufferSize)
 			// Decode a packet
-			packet := gopacket.NewPacket(iBuffer, layers.LayerTypeEthernet, gopacket.Default)
-			// Iterate over all layers, printing out each layer type
-			for _, layer := range packet.Layers() {
-			  fmt.Println("LAYER:", layer.LayerType())
+			packet := gopacket.NewPacket(iBuffer, layers.LayerTypeEthernet, gopacket.NoCopy)
+			if err := packet.ErrorLayer(); err != nil {
+				fmt.Println("Error decoding part of the packet:", err)
+			}
+
+			if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+				udpPacket, _ := udpLayer.(*layers.UDP)
+				udp_dgrams++
+				// Access UDP packet information
+				fmt.Printf("Source Port: %d\n", udpPacket.SrcPort)
+				fmt.Printf("Destination Port: %d\n", udpPacket.DstPort)
+				fmt.Printf("Payload: %x (%s)\n", udpPacket.Payload, udpPacket.Payload)
+				fmt.Println("--------------------------------------")
 			}
 	    }
+    	xdp_sock.RxBatchRelease(frames_rcvd)
 
-	     if stop {
+	    if stop {
 		 	break
-		 }
+		}
 	}
-    xdp_sock.RxBatchRelease(frames_rcvd)
+	fmt.Printf("\nUDP dgrams received: %d\n", udp_dgrams)
 }
